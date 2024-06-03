fecacc98209926003d545ed7bd9b26a3ab4dc1a5
jang-taehyun <jth0202@naver.com>
2024-06-03 21:57:27 +0900

    Lecture_16_Linux

---
+++ Linux/16_3_HalfCloseWithdupFunction/sep_serv2.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#define BUF_SIZE 1024
+
+void ErrorHandling(const char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+int main(int argc, char* argv[])
+{
+	int ServerSocket, ClientSocket;
+	struct sockaddr_in ServerAddress, ClientAddress;
+	socklen_t AddressSize;
+
+	FILE* readfp, * writefp;
+
+	char buf[BUF_SIZE];
+
+	int FunctionResult;
+
+	if(2 != argc)
+	{
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	ServerSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if(-1 == ServerSocket)
+		ErrorHandling("socket() error");
+
+	memset(&ServerAddress, 0, sizeof(ServerAddress));
+	ServerAddress.sin_family = AF_INET;
+	ServerAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+	ServerAddress.sin_port = htons(atoi(argv[1]));
+
+	FunctionResult = bind(ServerSocket, (struct sockaddr*)&ServerAddress, sizeof(ServerAddress));
+	if(-1 == FunctionResult)
+		ErrorHandling("bind() error");
+
+	FunctionResult = listen(ServerSocket, 5);
+	if(-1 == FunctionResult)
+		ErrorHandling("listen() error");
+
+	AddressSize = sizeof(ClientAddress);
+	ClientSocket = accept(ServerSocket, (struct sockaddr*)&ClientAddress, &AddressSize);
+	if(-1 == ClientSocket)
+		ErrorHandling("accept() error");
+
+	readfp = fdopen(ClientSocket, "r");
+	writefp = fdopen(dup(ClientSocket), "w");
+
+	fputs("FROM SERVER : Hi~ client?\n", writefp);
+	fputs("I love all of the world\n", writefp);
+	fputs("You are awesom!\n", writefp);
+	fflush(writefp);
+
+	shutdown(fileno(writefp), SHUT_WR);
+	fclose(writefp);
+
+	fgets(buf, sizeof(buf), readfp);
+	fputs(buf, stdout);
+
+	fclose(readfp);
+
+	return 0;
+}
